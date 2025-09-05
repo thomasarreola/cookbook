c397178ff4e9c10a3979bd1cce261b8b91623517
Thomas Arreola <thomasarreolaprofessional@gmail.com>
2025-09-04 18:50:32 -0700

    finished setting up tabs

---
+++ app/(tabs)/stock.tsx
@@ -0,0 +1,5 @@
+import { Text, View } from "react-native";
+
+export default function stock() {
+  return <View />;
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
-import { Tabs } from "expo-router";
+import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Tabs />;
-}
+const StackLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
+};
+
+export default StackLayout;
